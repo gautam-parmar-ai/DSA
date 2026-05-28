b395b79500b928d7238724b6e1710b485d5c05e9
Gautam Parmar <gautamparmarverona@gmail.com>
2026-05-28 18:06:40 +0200

    Time: 18 ms (56.76%) | Memory: 73.7 MB (71.53%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -1,7 +1,10 @@
 function containsDuplicate(arr){
     let seen = new Set()
     for(let i = 0; i < arr.length; i++){
+        if(seen.has(arr[i])){
+            return true;
+        }
         seen.add(arr[i])
     }
-    return (arr.length === seen.size ? false : true)
+    return false
 }
