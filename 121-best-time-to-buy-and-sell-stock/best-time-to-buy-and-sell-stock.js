3a4777528869a6178f8b1873509a8ca13f684889
Gautam Parmar <gautamparmarverona@gmail.com>
2026-05-29 10:41:11 +0200

    Time: 2 ms (80.52%) | Memory: 65.1 MB (62.81%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,10 @@
+function maxProfit(prices){
+    let minPrice = prices[0]
+    let maxProfit = 0
+
+    for(let i = 0; i < prices.length; i++){
+        minPrice = Math.min(minPrice, prices[i])
+        maxProfit = Math.max(maxProfit, (prices[i] - minPrice))
+    }
+    return maxProfit
+}
