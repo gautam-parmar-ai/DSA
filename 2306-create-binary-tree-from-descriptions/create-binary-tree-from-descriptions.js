8b601379aed710d02397b635cfb55e8d09d45a2e
Gautam Parmar <gautamparmarverona@gmail.com>
2026-06-07 23:34:31 +0200

    Time: 91 ms (41.94%) | Memory: 86.2 MB (29.03%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[][]} descriptions
+ * @return {TreeNode}
+ */
+var createBinaryTree = function(descriptions) {
+    const nodes = new Map();
+    const children = new Set();
+
+    for (const [parentVal, childVal, isLeft] of descriptions) {
+
+        if (!nodes.has(parentVal)) {
+            nodes.set(parentVal, new TreeNode(parentVal));
+        }
+
+        if (!nodes.has(childVal)) {
+            nodes.set(childVal, new TreeNode(childVal));
+        }
+
+        const parent = nodes.get(parentVal);
+        const child = nodes.get(childVal);
+
+        if (isLeft === 1) {
+            parent.left = child;
+        } else {
+            parent.right = child;
+        }
+
+        children.add(childVal);
+    }
+
+    for (const [value, node] of nodes) {
+        if (!children.has(value)) {
+            return node;
+        }
+    }
+};
